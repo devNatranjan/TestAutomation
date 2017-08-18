ad582be2a9a47eb41ed1eb30b66a35ede8118c66
dev.natranjan <dev.natranjan@trantorinc.com>
2017-08-18 12:48:59 +0530

    This is the test to check commit

--- ssss/src/Syso.java
+++ ssss/src/Syso.java
@@ -2,7 +2,10 @@ public class Syso {
 
     public static void main(String... args) {
 
-        System.out.println("my world ");
+      System.out.println("my world ");
+
+      System.out.println("This is the new line to ");
+
 
     }
 }
